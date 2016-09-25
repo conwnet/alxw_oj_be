c9e09d7f5e31784705de82a1c054ff4c8f6f53d8
netcon <netcon@live.com>
2016-09-25 16:43:09 +0800

    modify error

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -34,8 +34,7 @@ class User extends Controller
         $user = model\User::get(['username' => $username]);
         if($user == NULL) {
             $_POST['password'] = password_encrypt($_POST['password']);
-            $_POST['status'] = 1;
-            if((new model\User())->save(['username' => $username, 'password' => $password)) {
+            if((new model\User())->save(['username' => $username, 'password' => $password, 'status' => 1])) {
                 return json_encode(['error' => null]);
             } else {
                 return json_encode(['error' => 'db error']);
